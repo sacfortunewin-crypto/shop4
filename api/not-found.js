329bec9e13e0552837741c55876ff33834c1eb95
jacusi <jasuci@gmail.ci>
2026-06-08 23:58:34 -0300

    Replace shop4 with prepared folder

--- api/not-found.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.statusCode = 404;
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.end(JSON.stringify({ message: "Not found" }));
-};
